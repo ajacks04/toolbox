2538c86ec4c2fc2586a95ff707607fbdbf301985
Beres Jackson <beres.jackson@iceservices.com>
2021-02-22 15:35:35 +0100

    Error in test script fixed

--- tests/distance_test.py
+++ tests/distance_test.py
@@ -1,5 +1,5 @@
 # tests/distance_test.py
-from mlproject.distance import haversine
+from toolbox.distance import haversine
 
 def test_return():
     assert haversine(48.865070, 2.380009, 48.235070, 2.393409) != None
